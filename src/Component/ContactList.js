fd04db4e179f1245495e3caea96986438239a7fa
hwigyeompark <phg5590@gmail.com>
2018-05-14 23:41:28 +0900

    update ContactList

--- src/Component/ContactList.js
+++ src/Component/ContactList.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 
 class ContactList extends Component{
+    static defaultProps = {
+      noName : "No Name"
+    };
     render(){
         return(
-          <div></div>
+          <div>
+            <div>{this.props.noName}</div>
+          </div>
         );
     }
 }
