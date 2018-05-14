b9dedba3fc002c3bfa6e6469e4345dded221c42e
hwigyeompark <phg5590@gmail.com>
2018-05-14 23:40:22 +0900

    add ContactList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import ContactList from "./Component/ContactList";
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <ContactList/>
       </div>
     );
   }
